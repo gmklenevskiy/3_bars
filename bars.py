13ee27bce2ca40d2c021c3145f2f132f704cb7d7
gmklenevskiy <gmklenevskiy@gmail.com>
2017-08-06 17:57:23 +0300

    script is done

--- bars.py
+++ bars.py
@@ -1,21 +1,38 @@
 import json
-
+import os
+import sys
+import math
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open (filepath, 'r') as reader:
+        return json.load(reader)
 
 
 def get_biggest_bar(data):
-    pass
+    return max( data, key=lambda item: item['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    pass
-
+    return min( data, key = lambda item: item['SeatsCount'])
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    return min(data, key = lambda item: (item['geoData']['coordinates'][0] - longitude)**2 + (item['geoData']['coordinates'][1]-latitude)**2)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv)>1:
+        filepath = sys.argv[1]
+        reader = load_data(filepath)
+        biggest_bar = get_biggest_bar(reader)
+        print('The biggest bar \n', json.dumps(biggest_bar, indent = 4, ensure_ascii=False, sort_keys = True))
+        smallest_bar = get_smallest_bar(reader)
+        print('The smallest bar \n', json.dumps(smallest_bar, indent = 4, ensure_ascii=False, sort_keys = True))
+        print('Please enter your GeoData')
+        latitude = float(input('enter the latitude '))
+        longitude = float(input('enter the longitude '))
+        closest_bar = get_closest_bar(reader,longitude,latitude)
+        print('The closest bar \n', json.dumps(closest_bar, indent = 4, ensure_ascii = False, sort_keys = True))
+    else:
+        print("Please, enter the filepath")
